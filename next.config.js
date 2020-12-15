4e597a0ee24ea139f33e1c8369754f36a20c2ecd
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2020-12-15 19:00:53 +0200

    Exposed API routes blocked with redirect configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
 module.exports = {
   async redirects() {
     return [
+      /*
+       * Calling /stations/ without station_ID will redirect to stations list view.
+       */
       {
         source: "/stations",
+        destination: "/",
+        permanent: true,
+      },
+      /*
+       * Just an example on how to block unwanted routes.
+       * /pages/ should not have anything that is not an
+       * actual route  in a real life production application.
+       */
+      {
+        source: "/api/queries",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/api/resolvers",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/api/schemas",
         destination: "/",
         permanent: true,
       },
