383fb5091de4d1d1bf7090daf45e6cf5cdb196ef
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2020-12-15 19:13:08 +0200

    Cleanup and comments

--- pages/api/resolvers/index.js
+++ pages/api/resolvers/index.js
@@ -1,41 +1,17 @@
 import axios from "axios";
+/*
+ * ... or whatever your Ajax flavor is.
+ * This file is mostly warmed up copypasta with personal ketchup on top.
+ * In production, resolvers should filter out all data that is never queried from GraphQL.
+ */
 
 export const resolvers = {
   Query: {
-    getUsers: async () => {
-      try {
-        const users = await axios.get("https://api.github.com/users");
-        return users.data.map(({ id, login, avatar_url }) => ({
-          id,
-          login,
-          avatar_url,
-        }));
-      } catch (error) {
-        throw error;
-      }
-    },
-
-    getUser: async (_, args) => {
-      try {
-        const user = await axios.get(
-          `https://api.github.com/users/${args.name}`
-        );
-        return {
-          id: user.data.id,
-          login: user.data.login,
-          avatar_url: user.data.avatar_url,
-        };
-      } catch (error) {
-        throw error;
-      }
-    },
-
     getStations: async () => {
       try {
         const stations = await axios.get(
           "https://api.test.virtaglobal.com/stations/"
         );
-        // console.log(stations.data);
         return stations.data;
       } catch (error) {
         throw error;
@@ -47,6 +23,9 @@ export const resolvers = {
         const station = await axios.get(
           `https://api.test.virtaglobal.com/stations/${args.id}`
         );
+        // Optimization notes:
+        // const  {data,we,,dont,want,...rest} = station.data;
+        // return rest;
         return station.data;
       } catch (error) {
         throw error;
